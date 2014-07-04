8317f340e26d555891712e3829b3a7c69bf114c7
Nick Saika <nicksaika@gmail.com>
2014-07-03 20:47:30 -0400

    Fixed some small bugs.
    Things compile now!

--- main.go
+++ main.go
@@ -15,26 +15,26 @@ var (
 
 func main() {
 	listenAddr := flag.String("listen", "127.0.0.1:5000", "[address]:port to listen on")
-	mongoURL := flag.String("mongo", "mongodb://localhost:27017/eatem", "URL for the MongoDB database")
+	//mongoURL := flag.String("mongo", "mongodb://localhost:27017/eatem", "URL for the MongoDB database")
 	templatesDir := flag.String("templates", "./templates", "path to directory containing HTML templates")
 	staticDir := flag.String("static", "./static", "path to directory containing static files")
+	flag.Parse()
 
 	var err error
 
 	glog.Infoln("connecting to database")
 
 	glog.Infoln("loading templates")
-	glob := filepath.Join(*templatesDir, "*.html")
 	Templates, err = template.ParseGlob(filepath.Join(*templatesDir, "*.html"))
 	if err != nil {
 		glog.Fatalln(err)
 	}
 
-	http.Handle("/static/", http.FileServer(http.StripPrefix("/static", http.Dir(*staticDir))))
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir(*staticDir))))
 	http.HandleFunc("/index.html", handleIndex)
 
 	glog.Infoln("listening on", *listenAddr)
-	if err := http.ListenAndServe(*listenAddr); err != nil {
+	if err := http.ListenAndServe(*listenAddr, nil); err != nil {
 		glog.Fatalln(err)
 	}
 	glog.Infoln("terminated gracefully")
