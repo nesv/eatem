68c1fcfcaaac574e3dc3487231924616ae92439f
Nick Saika <nicksaika@gmail.com>
2014-08-28 17:56:01 -0400

    cmd/terf: load config file
    The terf command only takes two flags now (aside from those that are
    inherent when using glog):
    
    -config
    	Specify the path to the configuration file.
    -profile
    	The configuration profile to use.

--- cmd/terf/main.go
+++ cmd/terf/main.go
@@ -3,38 +3,66 @@ package main
 import (
 	"flag"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 	"path/filepath"
 
 	"github.com/golang/glog"
+	"github.com/hashicorp/hcl"
 )
 
 var (
 	Templates *template.Template
 )
 
+type (
+	Config map[string]ConfigProfile
+
+	ConfigProfile struct {
+		Terf TerfConfigBlock `json:"terf" hcl:"terf"`
+	}
+
+	TerfConfigBlock struct {
+		DatabaseURL string `json:"database_url" hcl:"database_url"`
+		ListenAddr  string `json:"listen_addr" hcl:"listen_addr"`
+		TemplateDir string `json:"template_dir" hcl:"template_dir"`
+		StaticDir   string `json:"static_dir" hcl:"static_dir"`
+	}
+)
+
 func main() {
-	listenAddr := flag.String("listen", "127.0.0.1:5000", "[address]:port to listen on")
-	//mongoURL := flag.String("mongo", "mongodb://localhost:27017/eatem", "URL for the MongoDB database")
-	templatesDir := flag.String("templates", "./templates", "path to directory containing HTML templates")
-	staticDir := flag.String("static", "./static", "path to directory containing static files")
+	configFile := flag.String("config", "terf.conf", "path to the terf configuration file")
+	configProfile := flag.String("profile", "development", "configuration profile to use")
 	flag.Parse()
 
-	var err error
+	// Load the configuration file.
+	var config Config
+	p, err := ioutil.ReadFile(*configFile)
+	if err != nil {
+		glog.Fatalln("failed to read config file:", err)
+	}
+	if err := hcl.Decode(&config, string(p)); err != nil {
+		glog.Fatalln("failed to decode config file:", err)
+	}
+	profile, ok := config[*configProfile]
+	if !ok {
+		glog.Fatalf("no such config profile %q", *configProfile)
+	}
 
 	glog.Infoln("connecting to database")
 
-	glog.Infoln("loading templates")
-	Templates, err = template.ParseGlob(filepath.Join(*templatesDir, "*.html"))
+	tplGlob := filepath.Join(profile.Terf.TemplateDir, "*.html")
+	glog.Infof("loading templates %q", tplGlob)
+	Templates, err = template.ParseGlob(tplGlob)
 	if err != nil {
 		glog.Fatalln(err)
 	}
 
-	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir(*staticDir))))
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir(profile.Terf.StaticDir))))
 	http.HandleFunc("/", handleIndex)
 
-	glog.Infoln("listening on", *listenAddr)
-	if err := http.ListenAndServe(*listenAddr, nil); err != nil {
+	glog.Infoln("listening on", profile.Terf.ListenAddr)
+	if err := http.ListenAndServe(profile.Terf.ListenAddr, nil); err != nil {
 		glog.Fatalln(err)
 	}
 	glog.Infoln("terminated gracefully")
